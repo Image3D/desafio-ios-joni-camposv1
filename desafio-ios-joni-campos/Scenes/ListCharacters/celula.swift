174856482e10c0085d429a1f95ba974372d881b6
Joni <jcampos@image3d.com.br>
2020-04-21 09:18:30 -0300

    First commit

---
+++ desafio-ios-joni-campos/Scenes/ListCharacters/celula.swift
@@ -0,0 +1,25 @@
+//
+//  celula.swift
+//  desafio-ios-joni-campos
+//
+//  Created by Joni de Campos on 20/04/20.
+//  Copyright © 2020 Joni de Campos. All rights reserved.
+//
+
+
+import UIKit
+
+class celula : UITableViewCell{
+    
+    @IBOutlet weak var nomeProduto: UILabel!
+    
+    
+    @IBOutlet weak var dataPedido: UILabel!
+    
+    
+    @IBOutlet weak var nomeCiente: UILabel!
+    
+    
+    @IBOutlet weak var characterImage: UIImageView!
+}
+
---
+++ desafio-ios-joni-campos/Services/Services.swift
@@ -0,0 +1,98 @@
+//
+//  Services.swift
+//  desafio-ios-joni-campos
+//
+//  Created by Joni de Campos on 20/04/20.
+//  Copyright © 2020 Joni de Campos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var oComics: CharacterDataWrapper?
+var oExpensive: CharacterComics?
+
+class Services {
+    
+    static func callAPIMarvelCharacters(completion: @escaping (_ result: CharacterDataWrapper ) ->() ){
+        // Marvel API Call
+        let jsonUrlString = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5ad9e048deff94a55ea68027047f675e&hash=3aea5e30d97860d6ca70af9474173c1f"
+
+
+        guard let url = URL(string: jsonUrlString)
+            else {return}
+
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+
+            DispatchQueue.main.async {
+                if let err = err {
+                    print("Failed to get data from url:", err)
+                    return
+                }
+
+                // Check if theres data returned
+                guard let data = data else{return}
+
+
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                // Parse JSON into Dictionary that contains Characters struct using JSONDecoder
+                guard let resultado = try? decoder.decode(CharacterDataWrapper.self, from: data) else {
+                    print("Error: Couldn't decode data into dictionary of array of characters")
+                    return
+                }
+
+                oComics = resultado
+                completion(resultado)
+                
+
+            }
+
+
+        }.resume()
+    }
+    
+    
+    static func callAPIMarvelCharactersID(completion: @escaping (_ result: CharacterComics ) ->() ){
+        // Marvel API Call
+        let jsonUrlString = "http://gateway.marvel.com/v1/public/characters/" + String(selectedPersonagemID) + "/comics?ts=1&apikey=5ad9e048deff94a55ea68027047f675e&hash=3aea5e30d97860d6ca70af9474173c1f"
+
+
+        guard let url = URL(string: jsonUrlString)
+            else {return}
+
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+
+            DispatchQueue.main.async {
+                if let err = err {
+                    print("Failed to get data from url:", err)
+                    return
+                }
+
+                // Check if theres data returned
+                guard let data = data else{return}
+
+
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                // Parse JSON into Dictionary that contains Characters struct using JSONDecoder
+                guard let resultadoID = try? decoder.decode(CharacterComics.self, from: data) else {
+                    print("Error: Couldn't decode data into dictionary of array of characters")
+                    return
+                }
+
+                oExpensive = resultadoID
+                completion(resultadoID)
+                
+
+            }
+
+
+        }.resume()
+    }
+    
+}
+
+
+
+
